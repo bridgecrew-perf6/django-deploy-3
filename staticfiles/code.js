9e1cdc20b91d5c8a06b02ca07c04175540bce1a4
mostafa faghih <faghih.606@gmail.com>
2022-05-24 22:32:59 +0300

    deploy-2

--- staticfiles/code.js
+++ staticfiles/code.js
@@ -6,3 +6,11 @@ const mobil_menu = document.querySelector('.menu')
 document.getElementById('icon').addEventListener('click', () => mobil_menu.classList.add('menu-open'))
 
 document.querySelector('.close-icon').addEventListener('click', () => mobil_menu.classList.remove('menu-open'))
+
+const zoom = document.querySelector('#zoom')
+const photo = document.querySelector('#photo')
+
+
+zoom.addEventListener('click', () => {
+    
+})
--- staticfiles/swiper.js
+++ staticfiles/swiper.js
@@ -7,9 +7,5 @@ var slideshow = new Swiper('.swiper-container', {
 
   loop : true ,
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
   keyboard: true,
 });
